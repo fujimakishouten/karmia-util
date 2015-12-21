4e926b48ae03c4428ed8073776235c3d9cab6a1a
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2015-12-21 15:27:56 +0900

    Change crypto utility to use buffer instead of text

--- test/crypto.js
+++ test/crypto.js
@@ -5,7 +5,7 @@
 
 
 // Variables
-var expect = require('expect.js'),
+const expect = require('expect.js'),
     utility = require('../');
 
 
@@ -14,53 +14,53 @@ describe('karmia-util', function () {
     describe('crypto', function () {
         describe('hash', function () {
             it('Should calculate md5 hash', function () {
-                var algorithm = 'md5',
-                    string = 'test';
+                const algorithm = 'md5',
+                    buffer = new Buffer('test');
 
-                expect(utility.crypto.hash(algorithm, string)).to.have.length(32);
+                expect(utility.crypto.hash(algorithm, buffer)).to.have.length(32);
             });
 
             it('Should calculate sha1 hash', function () {
-                var algorithm = 'sha1',
-                    string = 'test';
+                const algorithm = 'sha1',
+                    buffer = new Buffer('test');
 
-                expect(utility.crypto.hash(algorithm, string)).to.have.length(40);
+                expect(utility.crypto.hash(algorithm, buffer)).to.have.length(40);
             });
         });
 
         describe('sha256', function () {
             it('Should calculate sha256 hash', function () {
-                var string = 'test';
+                const buffer = new Buffer('test');
 
-                expect(utility.crypto.sha256(string)).to.have.length(64);
+                expect(utility.crypto.sha256(buffer)).to.have.length(64);
             });
         });
 
         describe('sha512', function () {
             it('Should calculate sha512 hash', function () {
-                var string = 'test';
+                const buffer = new Buffer('test');
 
-                expect(utility.crypto.sha512(string)).to.have.length(128);
+                expect(utility.crypto.sha512(buffer)).to.have.length(128);
             });
         });
 
         describe('encrypt', function () {
             it('Should encrypt string', function () {
-                var algorithm = 'aes-256-ctr',
+                const algorithm = 'aes-256-ctr',
                     password = 'password',
-                    string = 'test';
+                    buffer = new Buffer('test');
 
-                expect(utility.crypto.encrypt(algorithm, password, string)).to.have.length(8);
+                expect(utility.crypto.encrypt(algorithm, password, buffer)).to.have.length(4);
             });
         });
 
         describe('decrypt', function () {
             it('Should decrypt string', function () {
-                var algorithm = 'aes-256-ctr',
+                const algorithm = 'aes-256-ctr',
                     password = 'password',
-                    string = 'test',
-                    data = utility.crypto.encrypt(algorithm, password, string);
-                expect(utility.crypto.decrypt(algorithm, password, data)).to.be(string);
+                    buffer = new Buffer('test'),
+                    data = utility.crypto.encrypt(algorithm, password, buffer);
+                expect(utility.crypto.decrypt(algorithm, password, data)).to.eql(buffer);
             });
         });
     });
