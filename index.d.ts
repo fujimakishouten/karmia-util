dd19fd81c45e28aff3d1922947c34de757777614
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2018-04-03 03:56:57 +0000

    Fix d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,11 @@
-import KarmiaUtilityArray from "karmia-utility-array";
-import KarmiaUtilityCrypto from "karmia-utility-crypto";
-import KarmiaUtilityDate from "karmia-utility-date";
-import KarmiaUtilityObject from "karmia-utility-object";
-import KarmiaUtilityRandom from "karmia-utility-random";
-import KarmiaUtilityString from "karmia-utility-string";
-
-declare module KarmiaUtility {
-    export class KarmiaUtility {
-        array: KarmiaUtilityArray;
-        crypto: KarmiaUtilityCrypto;
-        date: KarmiaUtilityDate;
-        object: KarmiaUtilityObject;
-        random: KarmiaUtilityRandom;
-        string: KarmiaUtilityString;
-
-        constructor(options: Object|null|undefined);
-    }
+declare class KarmiaUtility {
+    array: any;
+    crypto: any;
+    date: any;
+    object: any;
+    random: any;
+    string: any;
 }
+
+declare function karmia_utility(object?: object): KarmiaUtility;
+export = karmia_utility;
